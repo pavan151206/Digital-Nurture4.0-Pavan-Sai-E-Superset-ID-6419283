8a17d1f784a5fce5a48735639086bd8a081d29c5
pavan151206 <pavan1512sai@gmail.com>
2025-06-29 18:07:41 +0530

    Add files via upload

---
+++ CognizantWeek2-File-1/TestProject1/TestProject1/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace MyApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+
+        public int Subtract(int a, int b) => a - b;
+
+        public int Multiply(int a, int b) => a * b;
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
---
+++ CognizantWeek2-File-1/TestProject1/TestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿namespace MyApp.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new Calculator(); 
+        }
+
+        [Test]
+        public void Add()
+        {
+            int result = calc.Add(5, 7);
+            Assert.AreEqual(12, result);
+        }
+
+        [Test]
+        public void Subtract()
+        {
+            int result = calc.Subtract(10, 4);
+            Assert.AreEqual(6, result);
+        }
+    }
+}
